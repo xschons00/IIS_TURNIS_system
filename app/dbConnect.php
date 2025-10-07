e6b01bc39ba8cc56f2949e78d5dc1940f52e3be8
pterodaktilus <xjakubk00@stud.fit.vutbr.cz>
2025-10-07 14:03:09 +0200

    fixed login to sql server

--- app/dbConnect.php
+++ app/dbConnect.php
@@ -4,10 +4,9 @@
 
 function start()
 {
-    echo "Start function called.\n";
     $db = mysqli_init();
-    if (!mysqli_real_connect($db, 'xjakubk00@eva.fit.vut.cz:', 'xjakubk00', 'timnu3ir', 'xjakubk00', 0, '/var/run/mysql/mysql.sock')) {
-	die('cannot connect '.mysqli_connect_error());
+    if (!mysqli_real_connect($db, 'localhost', 'xjakubk00', 'timnu3ir', 'xjakubk00', 0, '/var/run/mysql/mysql.sock')) {
+        die('cannot connect ' . mysqli_connect_error());
     }
 }
 start();
